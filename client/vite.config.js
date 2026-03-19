2f00b37f43adf2a6e23e57f34e79a7536d3ad546
Amine <aminchana.business@gmail.com>
2026-03-19 23:17:54 +0000

    fix:improve mobile responsiveness and add viewport meta tag for deployement

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
@@ -16,4 +16,4 @@ export default defineConfig({
       usePolling: true,
     },
   },
-})
+});
